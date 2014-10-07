ebb1ca767e1151e45edbbd3b3cb5263acbf1a550
= <pi@raspberrypi.(none)>
2014-10-06 23:59:01 -0700

    Added a hardware button that shuts down the raspberry pi and updated the rc.local file to support it.

---
+++ shutdown.py
@@ -0,0 +1,17 @@
+# Adam Anderson
+
+import RPi.GPIO as GPIO
+from subprocess import call
+
+
+def main():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.IN, GPIO.PUD_UP)
+
+    # Wait until someone hits the button
+    GPIO.wait_for_edge(17, GPIO.FALLING)
+    call(["sudo", "shutdown", "-hP", "now"])
+
+
+if __name__ == "__main__":
+    main()
